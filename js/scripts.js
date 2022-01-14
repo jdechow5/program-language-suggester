2c2b238730ca7eca332f68139d886141568a0a4a
Justin Dechow <justin.dechow@gmail.com>
2022-01-14 11:28:16 -0800

    add branching logic to all five questions

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,57 @@ $(document).ready(function () {
     const colorChoice = $("select#color").val();
     const locationChoice = $("select#studyLocation").val();
     const musicChoice = $("select#music").val();
-    const beverageChoice = $("select#beverage").val()
+    const beverageChoice = $("select#beverage").val();
+
+    let javaScriptCounter = 0;
+    let pythonCounter = 0;
+    let rustCounter = 0;
+
+    //Question 1 
+    if (animalChoice === "Dog") {
+      javaScriptCounter += 1;
+    } else if (animalChoice === "Cat") {
+      rustCounter += 1;
+    } else if (animalChoice === "Lizard") {
+      pythonCounter += 1;
+    }
+
+    //Question 2
+    if (colorChoice === "Blue") {
+      javaScriptCounter += 1;
+    } else if (colorChoice === "Green") {
+      rustCounter += 1;
+    } else if (colorChoice === "Purple") {
+      pythonCounter += 1;
+    }
+
+    //Question 3
+    if (locationChoice === "Home Office") {
+      javaScriptCounter += 1;
+    } else if (locationChoice === "A Coffee Shop") {
+      rustCounter += 1;
+    } else if (locationChoice === "The Library") {
+      pythonCounter += 1;
+    }
+
+    //Question 4
+    if (musicChoice === "Classical") {
+      javaScriptCounter += 1;
+    } else if (musicChoice === "Indie") {
+      rustCounter += 1;
+    } else if (musicChoice === "Metal") {
+      pythonCounter += 1;
+    }
+
+    //Question 5
+    if (beverageChoice === "Coffee") {
+      javaScriptCounter += 1;
+    } else if (beverageChoice === "Beer") {
+      rustCounter += 1;
+    } else if (beverageChoice === "Cocktail") {
+      pythonCounter += 1;
+    }
+
 
 
 
