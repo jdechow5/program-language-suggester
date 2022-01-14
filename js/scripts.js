f7a794221b7f25e02a3b4cac500e1ff5effb6d84
Justin Dechow <justin.dechow@gmail.com>
2022-01-14 10:58:26 -0800

    create jquery variables to extract values.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $("form#languageForm").submit(function (event) {
+    event.preventDefault();
+    const yourName = $("#name").val();
+    const animalChoice = $("select#animal").val();
+    const colorChoice = $("select#color").val();
+    const locationChoice = $("select#studyLocation").val();
+    const musicChoice = $("select#music").val();
+    const beverageChoice = $("select#beverage").val()
+
+
+
+
+  })
+})
